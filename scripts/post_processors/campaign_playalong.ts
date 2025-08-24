f8fd852aec4a0bd9b7e114f9549a58d276e3703c
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2025-08-24 22:34:11 +0200

    Update Campaign-Playalong pack

--- scripts/post_processors/campaign_playalong.ts
+++ scripts/post_processors/campaign_playalong.ts
@@ -1,4 +1,4 @@
-import type { Card } from "../lib/post_process_helpers";
+import type { Data } from "../lib/post_process_helpers";
 
 const CARD_POOL_EXTENSIONS = {
   "70b5bb78-8b12-40e4-a567-85f6996e836f": "card",
@@ -12,10 +12,19 @@ const CARD_POOL_EXTENSIONS = {
   "676974a0-aada-424f-b1bf-295c20782614": "card",
   "64cad566-0a68-4064-9a7f-7d6c321cdf40": "card",
   "a9cc4297-f55d-48f6-b955-5e30a9e38dfb": "card",
+  "90b02ba0-c18c-4d84-9e9a-72e2270697e1": "card",
+  "e122e372-8c4c-4a8d-98df-c710c93b3c36": "card",
+  "726c3e6f-fcbf-4f3e-9b57-c82897a1f27d": "card",
+  "3a6c43c8-79c9-43d8-bdc5-22a52d89a2b4": "card",
+  "e499384b-46a9-4663-9e4e-583ffefd8e08": "card",
+  "842af37e-2eb0-4c10-8153-2b8535af79fd": "card",
 };
 
-export default function mapper(cards: Card[]) {
-  return cards.map((card) => {
+export default function mapper(data: Data) {
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  const encounterSetCache: Record<string, any> = {};
+
+  const cards = data.cards.map((card) => {
     const type = CARD_POOL_EXTENSIONS[card.code];
     if (type) card.card_pool_extension = { type };
 
@@ -23,6 +32,33 @@ export default function mapper(cards: Card[]) {
       card.permanent = true;
     }
 
+    if (!card.encounter_code) {
+      const pack = data.packs.find((p) => p.code === card.pack_code);
+
+      const encounterSet = encounterSetCache[card.pack_code] || {
+        code: crypto.randomUUID(),
+        // biome-ignore lint/style/noNonNullAssertion: script
+        name: pack!.name,
+        // biome-ignore lint/style/noNonNullAssertion: script
+        icon_url: pack!.icon_url,
+      };
+
+      encounterSetCache[card.pack_code] = encounterSet;
+      card.encounter_code = encounterSet.code;
+      card.encounter_position = card.position;
+    }
+
+    // biome-ignore lint/performance/noDelete: script usage
+    delete card.xp;
+
     return card;
   });
+
+  const encounter_sets = Object.values(encounterSetCache);
+
+  return {
+    cards,
+    packs: data.packs,
+    encounter_sets: [...data.encounter_sets, ...encounter_sets],
+  };
 }
